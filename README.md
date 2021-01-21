172d4e3210481b6f4e9df0667f152efe76712c91
sana <takagisou@users.noreply.github.com>
2021-01-21 22:52:05 +0900

    add bgm

---
+++ README.md
@@ -0,0 +1,10 @@
+# Processing + Kotlin
+
+
+# BGM
+
+[SHW - 湖畔の村](http://shw.in/sozai/etc.php)
+
+# cf.
+
+http://r-dimension.xsrv.jp/classes_j/minim/
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,27 @@
+import ddf.minim.AudioPlayer
+import ddf.minim.Minim
 import processing.core.PApplet
 
 class Main: PApplet() {
+
+    private lateinit var minim: Minim
+    private lateinit var player: AudioPlayer
+
+    override fun setup() {
+        this.minim = Minim(this)
+        this.player = minim.loadFile("sound/bgm.mp3")
+        this.player.play()
+    }
+
+    override fun draw() {
+        background(0)
+    }
+
+    override fun stop() {
+        this.player.close() // サウンドデータ終了
+        this.minim.stop()
+        super.stop()
+    }
     fun run() = PApplet.main(Main::class.java.simpleName)
 }
 
