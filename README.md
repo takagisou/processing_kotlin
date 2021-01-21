3338821ac53c2515d90e476a8070ac60443a1931
sana <takagisou@users.noreply.github.com>
2021-01-22 01:01:05 +0900

    sine wave

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
 # Processing + Kotlin
 
 
+
 # BGM
 
 [SHW - 湖畔の村](http://shw.in/sozai/etc.php)
 
+[Free Game Development Resources > Retro Arcade Game Sounds](https://www.themotionmonkey.co.uk/free-resources/retro-arcade-sounds/)
+
 # cf.
 
+https://qiita.com/narikei/items/f09cd510bcb2dbb267cb
+
 http://r-dimension.xsrv.jp/classes_j/minim/
+
+http://30min-processing.hatenablog.com/entry/2015/12/04/000000
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,27 +1,89 @@
+import ddf.minim.AudioOutput
 import ddf.minim.AudioPlayer
 import ddf.minim.Minim
+import ddf.minim.ugens.Oscil
+import ddf.minim.ugens.Waves
 import processing.core.PApplet
+
+enum class Sound {
+    BGM,
+    SE;
+
+    val filePath: String
+        get() = when(this) {
+            BGM -> "sound/bgm.mp3"
+            SE -> "sound/gun.wav"
+        }
+}
 
 class Main: PApplet() {
 
     private lateinit var minim: Minim
+    private lateinit var output: AudioOutput
+    private lateinit var wave: Oscil
     private lateinit var player: AudioPlayer
 
     override fun setup() {
         this.minim = Minim(this)
-        this.player = minim.loadFile("sound/bgm.mp3")
-        this.player.play()
+        this.output = minim.lineOut
+
+        wave = Oscil(440f, 0.5f, Waves.SINE)
+        wave.patch(output)
+    }
+
+    override fun settings() {
+        size(512, 200)
     }
 
     override fun draw() {
-        background(0)
+        background(255)
+
+        stroke(255f, 0f, 0f)
+        strokeWeight(3f);
+        noFill()
+        beginShape()
+        for(i in 0 until width) {
+            vertex(i.toFloat(), height / 2 - wave.waveform.value(i.toFloat() / width) * 100)
+        }
+        endShape()
+
+        stroke(0)
+        strokeWeight(1f)
+        noFill()
+        beginShape()
+        for(i in 0 until output.bufferSize()) {
+            vertex(i.toFloat(), height / 2 + output.mix.get(i) * 100)
+        }
+        endShape()
     }
+
+    override fun mouseMoved() {
+        val amp = map(mouseY.toFloat(), 0f, height.toFloat(), 1f, 0f)
+        wave.setAmplitude(amp)
+        val freq = map(mouseX.toFloat(), 0f, width.toFloat(), 0f, 15000f)
+        wave.setFrequency(freq)
+    }
+
+    override fun mousePressed() = saveFrame("images/image.jpg")
 
     override fun stop() {
         this.player.close() // サウンドデータ終了
         this.minim.stop()
         super.stop()
     }
+
+    override fun keyPressed() {
+        wave.waveform = when(key) {
+            '1' -> Waves.SINE
+            '2' -> Waves.TRIANGLE
+            '3' -> Waves.SAW
+            '4' -> Waves.SQUARE
+            '5' -> Waves.QUARTERPULSE
+            '6' -> Waves.PHASOR
+            else -> Waves.SINE
+        }
+    }
+
     fun run() = PApplet.main(Main::class.java.simpleName)
 }
 
